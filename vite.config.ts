f18d28f7e81af8ac926c6632c8ebff53d3c7d843
sidnei silva <tjsidnei@gmail.com>
2025-08-05 10:24:02 +0100

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { federation } from "@module-federation/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -22,6 +23,41 @@ export default defineConfig({
 				},
 			},
 			shared: ["react", "react-dom", "react-router-dom"],
+		}),
+		VitePWA({
+			registerType: "autoUpdate",
+			injectRegister: "auto",
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,ico,png,svg}"], // Define quais ficheiros serão guardados em cache
+			},
+			manifest: {
+				name: "Territories PWA",
+				short_name: "Territories",
+				description: "A minha aplicação de gestão de territórios.",
+				theme_color: "#242424",
+				background_color: "#242424",
+				display: "standalone",
+				scope: "/",
+				start_url: "/",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+			},
 		}),
 	],
 	server: {
