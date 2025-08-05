e3da01fec0a71b9381105ca30a350fbaad50e217
sidnei silva <tjsidnei@gmail.com>
2025-08-05 10:49:54 +0100

    receive message to redirect to territories page after login success

--- src/pages/LoginWrapper.tsx
+++ src/pages/LoginWrapper.tsx
@@ -1,8 +1,37 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoginApp = React.lazy(() => import("loginApp/App"));
 
 const LoginWrapper = () => {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const handleMessage = (event: MessageEvent) => {
+			// Por segurança, verifique sempre a origem da mensagem
+			if (event.origin !== "https://territories-container.vercel.app") {
+				return;
+			}
+
+			// Verifique se a mensagem é a que esperamos
+			if (event.data && event.data.type === "LOGIN_SUCCESS") {
+				console.log(
+					"[Container] Mensagem de sucesso recebida do loginApp. Navegando..."
+				);
+				// Use o navigate do *container* para uma transição suave
+				navigate("/territories");
+			}
+		};
+
+		// Adicione o "ouvinte" de mensagens
+		window.addEventListener("message", handleMessage);
+
+		// Limpe o "ouvinte" quando o componente for desmontado
+		return () => {
+			window.removeEventListener("message", handleMessage);
+		};
+	}, [navigate]); // Adicione navigate como dependência
+
 	return (
 		<Suspense fallback={<div>Carregando login...</div>}>
 			<LoginApp />
