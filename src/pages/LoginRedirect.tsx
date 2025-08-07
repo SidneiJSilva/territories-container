6930836ab02ba7478caf2a7ceda1670a361eec07
sidnei silva <tjsidnei@gmail.com>
2025-08-07 10:55:01 +0100

    adjust to correct params

--- src/pages/LoginRedirect.tsx
+++ src/pages/LoginRedirect.tsx
@@ -7,7 +7,7 @@ const LoginRedirect = () => {
 	useEffect(() => {
 		const callBackUrl = "99a4b5e6-9746-4416-8c10-fc6e2afda3c6";
 		const appId = "98fcd818-0408-488e-81cc-022377aa5d07";
-		const callBackOrigin = "https://territories-container.vercel.app";
+		const callBackOrigin = "b00d1c4f-fbf6-434d-8f84-a3a175d303f2";
 
 		const pathToLogin = `/login?callBackUrl=${callBackUrl}&appId=${appId}&callBackOrigin=${callBackOrigin}`;
 
